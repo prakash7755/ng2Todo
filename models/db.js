2f4cc418a8ac0e0fd33bee51aa0819ed95b779aa
prakash7755 <mailmeprakash1992@gmail.com>
2018-02-03 15:24:14 +0530

    Mongoose connection

--- models/db.js
+++ models/db.js
@@ -1,17 +1,47 @@
 'use strict';
 
-const mongoose = require('mongoose');
-const uri = 'mongodb://localhost/todo';
-const useMongoClient = true;
-mongoose.Promise = global.Promise;
-mongoose.connect(uri, {useMongoClient}, (err)=> {
-	if (err) 
-		console.log('Database not connected');
+// const mongoose = require('mongoose');
+// const uri = 'mongodb://localhost/todo';
+// const useMongoClient = true;
+// mongoose.Promise = global.Promise;
+// mongoose.connect(uri, {useMongoClient}, (err)=> {
+// 	if (err) 
+// 		console.log('Database not connected');
 
-     else
-		console.log('Database  connected')
+//      else
+// 		console.log('Database  connected')
   
+// })
+
+
+// module.exports = mongoose
+
+'use strict';
+const mongoose = require('mongoose');
+const dbURI = 'mongodb://localhost/test';
+
+
+mongoose.connect(dbURI);
+
+mongoose.connection.on('connected', () => {
+	console.log('Mongoose default connection open to ' + dbURI);
 })
+
+mongoose.connection.on('error', (err) => {
+	console.log('Mongoose default error ' + err);
+})
+
+mongoose.connection.on('disconnected', (err) => {
+	console.log('Mongoose default disconnected ');
+})
+
+process.on('SIGINT', () => {
+	mongoose.onnection.close(() => {
+	console.log('Mongoose default connected ')
+      process.exit(0);
+	})
+})
+
 
 
 module.exports = mongoose
